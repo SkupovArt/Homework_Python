b98fdb9a88cae114ebc34242c11320f4d69a6ac2
SkupovArt <skripachmaster39@gmail.com>
2022-09-06 19:58:36 +0400

    выполнено

--- 04.09_Homework001.py
+++ 04.09_Homework001.py
@@ -1,2 +1,17 @@
-x = 'hello world'
-print(x)
+# Задание 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+# является ли этот день выходным.
+
+# Пример:
+
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+numberOfDay = int(input('Введите цифру дня недели '))
+week = [1, 2, 3, 4, 5, 6, 7]
+def DayX(num):
+    if num in week:
+        return True
+    else:
+        return False
+print(DayX(numberOfDay))
